853f62d9b9c6d0738e4e9448bb73f99dc948a36f
Julian Wöhrer <julian.woehrer@gmail.com>
2021-07-29 15:58:35 +0200

    do server in a while loop

--- tcpserver.py
+++ tcpserver.py
@@ -5,22 +5,22 @@ import sys
 
 HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-#while True:
-def server():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            #while True:
-            data = conn.recv(1024)
-            print(data.decode())
-            if not data:
-                pass #TODO
-            if data == b'':
-                pass #TODO
-            conn.sendall(data)
-    except:
-        print("Unexpected error typserver.py server(): " + str(sys.exc_info()))
+while True:
+    def server():
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                #while True:
+                data = conn.recv(1024)
+                print(data.decode())
+                if not data:
+                    pass #TODO
+                if data == b'':
+                    pass #TODO
+                conn.sendall(data)
+        except:
+            print("Unexpected error typserver.py server(): " + str(sys.exc_info()))
