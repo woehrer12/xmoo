157a25b87ce9d5671ca804015682c7a6a36654b2
Julian Wöhrer <julian.woehrer@gmail.com>
2021-09-29 21:59:54 +0200

    Gyro und Telemetrie

--- master.py
+++ master.py
@@ -1,4 +1,5 @@
 import TCPSERVER
+import TCPCLIENT
 import sys
 import BTS7960HBridgePCA9685 as HBridge
 import os
@@ -10,14 +11,21 @@ import time
 speedleft = 0
 speedright = 0
 
+programmstart = time.time()
+
 def printscreen():
     os.system('clear')
     print("========== Geschwindigkeitsanzeige ==========")
     print("Geschwindigkeit linker Motor:  ", speedleft)
     print("Geschwindigkeit rechter Motor: ", speedright)
+    if not TcpClient.connected:
+        print("Telemetrie not connected")
+    print(TcpServer.Ping)
     print(TcpServer.getDirection())
 
 #Abarbeitungsschleife
+
+TcpClient = TCPCLIENT.TCP_CLIENT()
 
 TcpServer = TCPSERVER.TCP()
 TcpServer.setHostPort('0.0.0.0',65432)
@@ -25,7 +33,12 @@ prozess = threading.Thread(target=TcpServer.server,args=())
 prozess.start()
 
 while True:
-    #printscreen()
+    if TcpServer.IP and not TcpClient.connected:
+        print(TcpServer.IP)
+        print(type(TcpServer.IP))
+        TcpClient.setHostPort(TcpServer.IP,65431)
+        TcpClient.connect()
+    printscreen()
     if(TcpServer.getDirection() == "UP"):
         speedleft = speedleft + 0.1
         speedright = speedright + 0.1
